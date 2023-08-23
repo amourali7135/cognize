8c04762c09cd0e91057dd721ff3e0925ba12ba84
Amir Mourali <amourali7135@gmail.com>
2023-08-24 03:54:33 +0800

    Controller work progress for MVP

--- app/controllers/badhabits_controller.rb
+++ app/controllers/badhabits_controller.rb
@@ -1,2 +1,63 @@
 class BadhabitsController < ApplicationController
+  before_action :set_badhabitlist, only: %i[show edit update destroy new create inex] # is this table even needed
+
+  before_action :set_badhabit, only: %i[show edit update destroy new create index]
+
+  def index
+    @badhabits = Badhabit.where(user_id: current_user.id)
+  end
+
+  def new
+    @badhabit = Badhabit.new
+  end
+
+  def edit
+    @badhabit = Badhabit.find(params[:id])
+  end
+
+  def create
+    @badhabit = Badhabit.new(badhabit_params)
+    @badhabit.badhabitlist = @badhabitlist
+    if @badhabit.save
+      redirect_to badhabitlist(@badhabitlist)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @badhabit = Badhabit.find(params[:id])
+    @badhabit.destroy
+    flash[:notice] = "Your badhabit was successfully deleted!"
+    redirect_to badhabitlist(@badhabitlist)
+  end
+
+  def update
+    @badhabit = Badhabit.find(params[:id])
+    if @badhabit.update(badhabit_params)
+      flash[:notice] = "Your badhabit was successfully updated"
+      redirect_to badhabitlist(@badhabitlist)
+    else
+      flash[:error] = "There was an error, please try again!"
+      render "edit"
+    end
+  end
+
+  def show
+    @badhabit = Badhabit.find(params[:id])
+  end
+
+  private
+
+  def set_badhabitlist
+    @badhabitlist = Badhabitlist.find(params[:regretlist_id])
+  end
+
+  def set_badhabit
+    @badhabit = Badhabit.find(params[:id])
+  end
+
+  def badhabit_params
+    params.require(:badhabit).permit(:title, :description, :reason, :initiate, :priority, :obstacles, :current_status)
+  end
 end
--- app/controllers/currentlegacy_controller.rb
+++ app/controllers/currentlegacy_controller.rb
@@ -1,2 +1,41 @@
 class CurrentlegacyController < ApplicationController
+  before_action :set_currentlegacy, only: %i[show edit update destroy new create index]
+
+  def new
+    @currentlegacy = Currentlegacy.new
+  end
+
+  def create
+    @currentlegacy = Currentlegacy.new(current_legacy_params)
+    @currentlegacy.user_id = current_user.id
+    if @currentlegacy.save
+      redirect_to present_currentlegacy_path(@currentlegacy)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @currentlegacy = Currentlegacy.find(params[:id])
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def show
+    @currentlegacy = Currentlegacy.find(params[:id])
+  end
+
+  private
+
+  def set_currentlegacy
+    @currentlegacy = Currentlegacy.find(params[:id])
+  end
+
+  def current_legacy_params
+    params.require(:currentlegacy).permit(:memorialized, :epitaph, :remembered_for)
+  end
 end
